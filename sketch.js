3b17eb1ecb3dc23c34376ecfee3967b38f694a66
ClaudiaDini <cdini9325@gmail.com>
2025-06-03 13:51:05 +0200

    prova

--- sketch.js
+++ sketch.js
@@ -1,47 +1,32 @@
-let video;
-
-/** @type {ml5.HandPose} */
-let handPose;
-
-/** @type {ml5.Hand[]} */
-let hands = [];
+let img_1;
+let img_2;
 
 function preload() {
-  // Load the handPose model
-  handPose = ml5.handPose({
-    flipped: true,
-    maxHands: 2,
-  });
+  img_2 = loadImage("A/A-1-18.svg");
+  img_1 = loadImage("A/A-2-19.svg");
 }
 
 function setup() {
-  const scale = 2;
-  createCanvas(640 * scale, 480 * scale);
-
-  // Create the webcam video and hide it
-  video = createCapture(VIDEO, { flipped: true });
-  video.size(width, height);
-  video.hide();
-
-  // start detecting hands from the webcam video
-  handPose.detectStart(video, function (results) {
-    hands = results;
-  });
+  createCanvas(200, 200);
+  imageMode(CENTER);
 }
 
 function draw() {
-  // Draw the webcam video
-  image(video, 0, 0, width, height);
+  background(0);
 
-  //
+  let larghezza_1 = map(mouseX, 0, width, 0, img_1.width);
 
-  const rightHand = hands.find((hand) => hand.handedness === "Right");
-  const leftHand = hands.find((hand) => hand.handedness === "Left");
-  if (!rightHand || !leftHand) return;
+  translate(width / 2, height / 2);
 
-  const punto = rightHand.index_finger_tip;
-  ellipse(punto.x, punto.y, 10, 10);
+  image(img_2, 0, 0);
 
-  const ditoMedioSx = leftHand.middle_finger_tip;
-  ellipse(ditoMedioSx.x, ditoMedioSx.y, 10, 10);
+  push();
+
+  beginClip();
+  rect(-img_1.width / 2, -img_1.height / 2, larghezza_1, img_1.height);
+  endClip();
+
+  image(img_1, 0, 0);
+
+  pop();
 }
